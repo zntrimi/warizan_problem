aba6ce8881c7803a4c708f0d7cd744c6e44c1454
Zentaro <zentaro0824@gmail.com>
2022-07-25 20:27:57 +0900

    lesgoo

--- calc.py
+++ calc.py
@@ -1,11 +1,10 @@
-x = 95
-y = 2
-s = 0
-r = 0
-i = 1
+x = 0 #割られる数 二桁
+y = 0 #割る数 一桁
+s = 0 #答え 二桁
+r = 0 #あまり 一桁
 
 for y in range(1, 10):
-    for x in range(1, 100):
+    for x in range(11, 100):
 
         x2 = 100 #仮の値
         s2 = 101 # 仮の値
@@ -25,7 +24,7 @@ for y in range(1, 10):
         if int(s) >= 10:
             s2 = int(s[-2])
 
-        if x1!=x2 and x1!=y and x1!=s1 and x1!=s2 and x1!=r and x2!=y and x2!=s1 and x2!=s2 and x2!=r and y!=s1 and y!=s2 and y!=r and s1!=s2 and s1!=r and s2!=r:
+        if x1!=x2 and x1!=y and x1!=s1 and x1!=s2 and x1!=r and x2!=y and x2!=s1 and x2!=s2 and x2!=r and y!=s1 and y!=s2 and y!=r and s1!=s2 and s1!=r and s2!=r and r!=0 and s2!=101:
             print(int(x),y, s, r)
 
 
